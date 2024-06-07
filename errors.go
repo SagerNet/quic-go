6344b7d2e2586a150fffac8798d85c0e07d9b561
世界 <i@sekai.icu>
2024-06-07 16:37:06 +0800

    Revert "fix: MaxDatagramSize changes"

--- errors.go
+++ errors.go
@@ -64,12 +64,12 @@ func (e *StreamError) Error() string {
 
 // DatagramTooLargeError is returned from Connection.SendDatagram if the payload is too large to be sent.
 type DatagramTooLargeError struct {
-	MaxDataLen int64
+	MaxDatagramPayloadSize int64
 }
 
 func (e *DatagramTooLargeError) Is(target error) bool {
 	t, ok := target.(*DatagramTooLargeError)
-	return ok && e.MaxDataLen == t.MaxDataLen
+	return ok && e.MaxDatagramPayloadSize == t.MaxDatagramPayloadSize
 }
 
 func (e *DatagramTooLargeError) Error() string { return "DATAGRAM frame too large" }
