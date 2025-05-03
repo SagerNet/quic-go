769b39dd18ca6ffceceacc0001eb5b9eec7e0e52
世界 <i@sekai.icu>
2025-05-03 13:49:44 +0800

    Rename module

--- framer.go
+++ framer.go
@@ -5,12 +5,12 @@ import (
 	"sync"
 	"time"
 
-	"github.com/quic-go/quic-go/internal/ackhandler"
-	"github.com/quic-go/quic-go/internal/flowcontrol"
-	"github.com/quic-go/quic-go/internal/protocol"
-	"github.com/quic-go/quic-go/internal/utils/ringbuffer"
-	"github.com/quic-go/quic-go/internal/wire"
-	"github.com/quic-go/quic-go/quicvarint"
+	"github.com/sagernet/quic-go/internal/ackhandler"
+	"github.com/sagernet/quic-go/internal/flowcontrol"
+	"github.com/sagernet/quic-go/internal/protocol"
+	"github.com/sagernet/quic-go/internal/utils/ringbuffer"
+	"github.com/sagernet/quic-go/internal/wire"
+	"github.com/sagernet/quic-go/quicvarint"
 )
 
 const (
@@ -209,7 +209,7 @@ func (f *framer) appendControlFrames(
 // This is a hack.
 // It is easier to implement than propagating an error return value in QueueControlFrame.
 // The correct solution would be to queue frames with their respective structs.
-// See https://github.com/quic-go/quic-go/issues/4271 for the queueing of stream-related control frames.
+// See https://github.com/sagernet/quic-go/issues/4271 for the queueing of stream-related control frames.
 func (f *framer) QueuedTooManyControlFrames() bool {
 	return f.queuedTooManyControlFrames
 }
