be116096b9370fb98cd7374245bc1db111c557b7
Haruue <i@haruue.moe>
2025-10-23 21:52:19 +0800

    fix: memory leak in h3 stream hijack

--- http3/server.go
+++ http3/server.go
@@ -536,6 +536,7 @@ func (s *Server) handleConn(conn quic.Connection) error {
 			// or the underlying connection is closed
 			defer wg.Done()
 			s.handleRequest(hconn, str, datagrams, hconn.decoder)
+			hconn.clearStream(str.StreamID())
 		}()
 	}
 	wg.Wait()
